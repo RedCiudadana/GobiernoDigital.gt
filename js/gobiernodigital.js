20405440acd879026892de069d2279752646e32b
Julio Herrera <julio@redciudadana.org>
2020-06-25 23:22:44 -0600

    Update GD

--- js/gobiernodigital.js
+++ js/gobiernodigital.js
@@ -1,3 +1,11 @@
+function nav_menu() {
+    var element = document.getElementById("nav_container");
+    var icon = document.getElementById("nav_icon");
+    
+    element.classList.toggle("show-menu");
+    icon.classList.toggle("show-icon");
+}
+
 $(document).ready(function() {
     
     $('.slider').slick({
